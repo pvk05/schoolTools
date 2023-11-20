fb4136a54d2e02cdef3f57c50ff488dbfa8aeb1c
pvk05 <philip.v.knudsen@gmail.com>
2023-11-20 12:38:27 +0100

    changed fagValg input to select

--- src/routes/minSide/fagValg.svelte
+++ src/routes/minSide/fagValg.svelte
@@ -1,6 +1,27 @@
 <script>
 
-	
+	let blokkAFag = [
+		"R2",
+		"S2"
+	]
+	let blokkBFag = [
+		"Fysikk 2",
+		"Engelsk",
+		"Politikk og Menneskerettigheter",
+		"Biologi 2",
+		"Sosialkunnskap"
+	]
+	let blokkCFag = [
+		"Kjemi 2",
+		"Psykologi"
+	]
+	let blokkDFag = [
+		"Tysk",
+		"Samfunnsgeografi",
+		"Kunst og visuelle virkemidler",
+		"Spansk III",
+		"Historie og filosofi"
+	]
 
 </script>
 
@@ -9,10 +30,37 @@
 <div class="form-widget bg-surface-200-700-token w-2/3 p-5 m-auto rounded">
 	<h3 class="h3">Fagvalg</h3>
 	<form action="?/fagValg" method="post">
-		<input type="text" name="blokkA" id="blokkA" class="input w-1/5 h-8 text-lg p-2 bg-secondary-300-600-token">
-		<input type="text" name="blokkB" id="blokkB" class="input w-1/5 h-8 text-lg p-2 bg-secondary-300-600-token">
-		<input type="text" name="blokkC" id="blokkC" class="input w-1/5 h-8 text-lg p-2 bg-secondary-300-600-token">
-		<input type="text" name="blokkD" id="blokkD" class="input w-1/5 h-8 text-lg p-2 bg-secondary-300-600-token">
-		<input type="submit" value="Submit">
+		<select name="blokkA" id="blokkA" class="variant-ghost-secondary select my-1">
+			<option value="" hidden>Velg et fag</option>
+			<option value="">Ingen fag</option>
+			{#each blokkAFag as fagA}
+				<option value="{fagA}" class="">{fagA}</option>
+			{/each}
+		</select>
+		<br>
+		<select name="blokkB" id="blokkB" class="variant-ghost-secondary select my-1">
+			<option value="" hidden>Velg et fag</option>
+			<option value="">Ingen fag</option>
+			{#each blokkBFag as fagB}
+				<option value="{fagB}">{fagB}</option>
+			{/each}
+		</select>
+		<br>
+		<select name="blokkC" id="blokkC" class="variant-ghost-secondary select my-1">
+			<option value="" hidden>Velg et fag</option>
+			<option value="">Ingen fag</option>
+			{#each blokkCFag as fagC}
+				<option value="{fagC}">{fagC}</option>
+			{/each}
+		</select>
+		<br>
+		<select name="blokkD" id="blokkD" class="variant-ghost-secondary select my-1">
+			<option value="" hidden>Velg et fag</option>
+			<option value="">Ingen fag</option>
+			{#each blokkDFag as fagD}
+				<option value="{fagD}">{fagD}</option>
+			{/each}
+		</select>
+		<input type="submit" value="Submit" class="btn variant-ghost-primary">
 	</form>
 </div>
